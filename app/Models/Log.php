3addb7073b0d8e40a292f90662e6405abe62d5bd
Rafael Breno <rafael_breno@outlook.com>
2021-02-12 20:19:45 -0300

    Add Log model and migration

---
+++ app/Models/Log.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'request_id', 'entity_id', 'response_id',
+        'route_id', 'service_id', 'latency_id',
+
+        'upstream_uri', 'client_ip', 'started_at',
+    ];
+}
---
+++ database/migrations/2021_02_12_225524_create_logs_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            // request
+            $table->foreignId('request_id');
+            // upstream_uri
+            $table->string('upstream_uri');
+            // response
+            $table->foreignId('response_id');
+            // authenticated_entities
+            $table->foreignId('entity_id');
+            // route
+            $table->foreignId('route_id');
+            // service
+            $table->foreignId('service_id');
+            // latency
+            $table->foreignId('latency_id');
+            // client_ip
+            $table->ipAddress('client_ip');
+            // started_at
+            $table->dateTime('started_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}

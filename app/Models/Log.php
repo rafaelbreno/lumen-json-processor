bbf47263aa8387f8eb66964a047b60a9f241bd85
Rafael Breno <rafael_breno@outlook.com>
2021-02-12 20:53:48 -0300

    Add table attribute into Log's model

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -9,6 +9,13 @@ use Illuminate\Database\Eloquent\Model;
 class Log extends Model
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'logs';
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array

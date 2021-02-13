3d1fc04a7b424ca239f6ee8f95f2d18497e8525d
Rafael Breno <rafael_breno@outlook.com>
2021-02-12 21:42:29 -0300

    Add entity model and migration

---
+++ app/Models/Entity.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Entity extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'entities';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'consumer_id'
+    ];
+}

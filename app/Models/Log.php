35117bbcfc7d1adc2e81e9288331a0b5c6fcbcf6
Rafael Breno <rafael_breno@outlook.com>
2021-02-12 22:37:29 -0300

    Add relationships into Log model

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -5,6 +5,7 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Log extends Model
 {
@@ -26,4 +27,36 @@ class Log extends Model
 
         'upstream_uri', 'client_ip', 'started_at',
     ];
+
+    // Relationships
+
+    public function latency(): HasOne
+    {
+        return $this->hasOne(Latency::class, 'latency_id', 'id');
+    }
+
+    public function service(): HasOne
+    {
+        return $this->hasOne(Service::class, 'service_id', 'id');
+    }
+
+    public function route(): HasOne
+    {
+        return $this->hasOne(Route::class, 'route_id', 'id');
+    }
+
+    public function response(): HasOne
+    {
+        return $this->hasOne(Response::class, 'response_id', 'id');
+    }
+
+    public function entity(): HasOne
+    {
+        return $this->hasOne(Entity::class, 'entity_id', 'id');
+    }
+
+    public function request(): HasOne
+    {
+        return $this->hasOne(Request::class, 'request_id', 'id');
+    }
 }

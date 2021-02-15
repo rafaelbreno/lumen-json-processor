9fa3699409daaca1324fdbbce4ed65a89ef47ca7
Rafael Breno <rafael_breno@outlook.com>
2021-02-14 22:02:38 -0300

    Add insertJson job into app's scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\InsertEachJson;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
@@ -19,11 +20,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        $schedule
+            ->job(InsertEachJson::class)
+            ->everyFifteenMinutes();
     }
 }

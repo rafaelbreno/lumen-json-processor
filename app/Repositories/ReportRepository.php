662d30bb1f5062706e38177f78ba6a352e71cfa5
Rafael Breno <rafael_breno@outlook.com>
2021-02-17 13:25:16 -0300

    Add consulting methods with raw DB queries

--- app/Repositories/ReportRepository.php
+++ app/Repositories/ReportRepository.php
@@ -11,16 +11,60 @@ class ReportRepository implements ReportRepositoryInterface
 {
     public function requestPerConsumer(Request $request): JsonResponse
     {
-        return response()->json();
+        // Raw SQL faster than Eloquent
+        $data = \DB::select("
+            SELECT
+                   consumer_id,
+                   COUNT(id) as quantity
+            FROM entities
+            GROUP BY consumer_id
+        ");
+//        $test = Entity::all()
+//            ->groupBy('consumer_id')
+//            ->map(function ($value, $key) {
+//                return count($value);
+//            })
+//            ->toArray();
+
+        return response()->json($data);
     }
 
     public function requestPerService(Request $request): JsonResponse
     {
-        return response()->json();
+        $data = \DB::select("
+            SELECT
+                   service_id,
+                   COUNT(id) as quantity
+            FROM logs
+            GROUP BY service_id
+        ");
+
+//        $test = Log::all()
+//            ->groupBy('service_id')
+//            ->map(function ($value, $key) {
+//                return count($value);
+//            })
+//            ->toArray();
+        return response()->json($data);
     }
 
     public function averageLatencyPerService(Request $request): JsonResponse
     {
-        return response()->json();
+        $data = \DB::select("
+            SELECT
+                services.id,
+                AVG(latencies.proxy) as avg_proxy,
+                AVG(latencies.gateway) avg_gateway,
+                AVG(latencies.request) avg_request
+            FROM services
+            INNER JOIN logs
+                ON services.id = logs.service_id
+            INNER JOIN latencies
+                ON logs.latency_id = latencies.id
+            WHERE services.id = logs.service_id
+            GROUP BY id
+        ");
+
+        return response()->json($data);
     }
 }

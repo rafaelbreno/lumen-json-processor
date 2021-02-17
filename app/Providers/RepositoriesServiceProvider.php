1704e69ed2eba3d9f7b2dd8843b29b4aa41c7baf
Rafael Breno <rafael_breno@outlook.com>
2021-02-16 21:56:06 -0300

    Bind ReportRepository

--- app/Providers/RepositoriesServiceProvider.php
+++ app/Providers/RepositoriesServiceProvider.php
@@ -8,6 +8,8 @@ use App\Interfaces\ImportFileRepositoryInterface;
 use App\Interfaces\LogRepositoryInterface;
 use App\Repositories\ImportFileRepository;
 use App\Repositories\LogRepository;
+use App\Repositories\ReportRepository;
+use App\Repositories\ReportRepositoryInterface;
 
 class RepositoriesServiceProvider extends \Illuminate\Support\ServiceProvider
 {
@@ -36,6 +38,11 @@ class RepositoriesServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->app->bind(
             ImportFileRepositoryInterface::class,
             ImportFileRepository::class
+        );
+
+        $this->app->bind(
+            ReportRepositoryInterface::class,
+            ReportRepository::class
         );
     }
 }
